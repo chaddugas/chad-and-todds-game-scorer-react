14663b9391238f8e8e1660ec479218c78abcb686
Chad Dugas <chad.dugas@icloud.com>
2024-07-19 11:40:33 -0600

    add PWA for offline mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './main.scss';
 
+// Register the service worker
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,65 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { VitePWA } from 'vite-plugin-pwa';
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
+const localCertPath = path.resolve(__dirname, '192.168.3.45+2.pem');
+const localKeyPath = path.resolve(__dirname, '192.168.3.45+2-key.pem');
+
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicons/favicon.ico', 'favicons/apple-touch-icon.png'],
+      manifest: {
+        name: "Chad and Todd's Game Scorer",
+        short_name: 'Game Scorer',
+        description: 'An app to score games offline',
+        theme_color: 'auto',
+        icons: [
+          {
+            src: 'favicons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+              },
+            },
+          },
+          {
+            urlPattern: /.*\.(?:js|css)/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources',
+            },
+          },
+        ],
+      },
+    }),
+  ],
   server: {
-    https: true,
+    host: '192.168.3.45',
+    https: {
+      key: fs.readFileSync(localKeyPath),
+      cert: fs.readFileSync(localCertPath),
+    },
   },
 });
